c41bdf2035ebaef2be5141d261f33f5fed74177a
Shueny <shuen29@gmail.com>
2019-05-09 16:35:43 +0800

    update code

---
+++ src/components/TodoList.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <div class="list-group-item text-left">
+        <div class="d-flex">
+          <div class="form-check">
+            <label class="form-check-label" :class="{'completed': item.completed }">{{ item.title }}
+              <input class="form-check-input" type="checkbox"
+              v-model="item.completed" :id="item.id"/>
+              <span class="checkmark"></span>
+            </label>
+          </div>
+          <button type="button" class="btn btn-default btn-edit mx-1" @click="editTodo(item)"
+          data-toggle="modal" data-target="#editModal">
+            <font-awesome-icon icon="edit"/>
+          </button>
+          <button class="btn close ml-auto mx-1" type="button"
+          aria-label="Close" @click="removeTodo(item)">
+            <font-awesome-icon icon="trash-alt" />
+          </button>
+          <button class="btn star ml-auto" :class="{'active': item.marked }"
+          type="button" aria-label="" @click="markTodos(item)">
+            <font-awesome-icon icon="star" />
+          </button>
+        </div>
+        <div class="pl-4 noteArea">
+          <span class="date px-2">
+            <b class="icon"><font-awesome-icon :icon="['far','calendar-check']"/></b>
+            {{ item.messageDate }}
+          </span>
+          <span class="comment-count px-2">
+            <b class="icon"><font-awesome-icon :icon="['far','comment-dots']"/></b>
+            {{ item.comments.length }}
+          </span>
+        </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TodoList',
+  props: ['item'],
+  data () {
+    return {
+      cacheTodo: {},
+      cacheTodoTitle: ''
+    }
+  },
+  methods: {
+    cancelEdit (item) {
+      this.$emit('cancel-edit', item)
+    },
+    removeTodo (item) {
+      this.$emit('remove-todo', item)
+    },
+    editTodo (item) {
+      this.$emit('edit-todo', item)
+    },
+    doneEdit (item) {
+      this.$emit('done-edit', item)
+    },
+    deleteTodos (item) {
+      this.$emit('delete-todo', item)
+    },
+    markTodos (item) {
+      this.$emit('mark-todo', item)
+    }
+  }
+}
+</script>
---
+++ src/views/Todo.vue
@@ -0,0 +1,241 @@
+<template>
+  <div class="app-todo">
+    <div class="container">
+      <section class="header">
+        <div class="text">
+          <span class="day" v-text="day"> </span>
+          <span class="yearMonth"><b class="year" v-text="year"></b><b class="month" v-text="month"> </b></span>
+          <span v-text="timeMessage"></span><span class="week" v-text="currentWeek"> </span>
+        </div>
+      </section>
+      <section class="addTask">
+        <input placeholder="+ Add task" v-model="newTodo" @keyup.enter="addTodo"/>
+          <a class="btn btn--add" @click="addTodo">+</a>
+      </section>
+      <section class="content">
+        <div>
+          <div class="todoList">
+            <TodoList
+             v-for='item in filteredTodos'
+             :key='item.index'
+             :item='item'
+             @cancel-item='cancelEdit'
+             @remove-todo='removeTodo'
+             @edit-todo='editTodo'
+             @done-edit='done-edit'
+             @delete-todo='deleteTodos'
+             @mark-todo='markTodos'
+            ></TodoList>
+          </div>
+        </div>
+      </section>
+      <div class="filters">
+        <button class="btn filters__btn filters__btn--all p-2" :class="{'active': visibility == 'all'}"
+        @click="visibility = 'all'">All</button>
+        <button class="btn filters__btn filters__btn--complete p-2" :class="{'active': visibility == 'completed'}"
+        @click="visibility = 'completed'">Complete</button>
+        <button class="btn filters__btn filters__btn--incomplete p-2" :class="{'active': visibility == 'doing'}"
+        @click="visibility = 'doing'">Incomplete</button>
+      </div>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content container">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Edit</h5>
+          </div>
+          <div class="modal-body">
+            <div class="mb-3">
+              <input class="form-control" type="text" v-model="cacheTodoTitle"
+              @keyup.esc="cancelEditComment()" @keyup.enter="doneEditComment(item)"/>
+            </div>
+            <div class="">
+              <p class="title text-left">Comments:</p>
+              <input class="form-control" type="text" placeholder="Add Comment..."
+              @keyup.esc="cancelAddComment()" @keyup.enter="addComment()" v-model="commentText"/>
+              <div class="comment-list m-2 py-2" v-for="(data, index) in cacheTodo.comments" :key="data.index" :class="{'border-bottom' : index !== (cacheTodo.comments.length-1)}">
+                <div class="d-flex px-3">
+                  <span>{{ data }}</span>
+                  <button class="btn close ml-auto mx-1" type="button" aria-label="Close" @click="removeComment(index)">
+                    <font-awesome-icon icon="trash-alt" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer justify-content-center">
+            <button type="button" class="btn btn-secondary btn--save" data-dismiss="modal">Close & Save</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scope>
+@import '@/assets/scss/_todo.scss';
+</style>
+
+<script>
+import TodoList from '@/components/TodoList.vue'
+import Datepicker from 'vuejs-datepicker'
+
+const moment = require('moment')
+
+const STORAGE_KEY = 'todos-vuejs'// 名稱
+var todoStorage = {
+  fetch () {
+    const todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+    todos.forEach((todo, index) => {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save (todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
+
+export default {
+  name: 'Todo',
+  components: { TodoList, Datepicker },
+  data () {
+    return {
+      newTodo: '',
+      todos: [
+        {
+          title: '要買蘿蔔',
+          completed: false,
+          marked: false,
+          messageDate: '05/02/2019 10:00 AM',
+          comments: ['6:00pm', 'the new restaurant']
+        },
+        {
+          title: '冷萃咖啡',
+          completed: true,
+          marked: true,
+          comments: [],
+          messageDate: '03/22/2019 08:23 AM'
+        }
+      ],
+      cacheTodo: {},
+      cacheTodoTitle: '',
+      cacheTodoMark: '',
+      cacheComment: {},
+      cacheCommentTitle: '',
+      commentText: '',
+      visibility: 'all',
+      day: moment().format('DD'),
+      year: moment().format('YYYY'),
+      month: moment().format('MMM'),
+      currentWeek: moment().format('ddd'),
+      timeMessage: moment().format('LTS')
+    }
+  },
+  methods: {
+    getTodos () {
+      if (localStorage.getItem('todos-vuejs')) {
+        this.todos = JSON.parse(localStorage.getItem('todos-vuejs'))
+      }
+    },
+    addTodo (e) {
+      // validation check
+      if (this.newTodo) {
+        this.todos.unshift({
+          id: this.todos.length,
+          title: this.newTodo,
+          completed: false,
+          marked: false,
+          comments: [],
+          messageDate: moment().format('L') + " " + moment().format('LT')
+        })
+      }
+      // reset newTodo
+      this.newTodo = ''
+      // save the new item in localstorage
+      return true
+    },
+    updateCurrentTime () {
+      this.timeMessage = moment().format('LTS')
+    },
+    cancelEdit () {
+      this.cacheTodo = {}
+    },
+    removeTodo (item) {
+      const delIndex = this.todos.indexOf(item)
+      this.todos.splice(delIndex, 1)
+    },
+    editTodo (item) {
+      // console.log(item.title)
+      this.cacheTodo = item
+      this.cacheTodoTitle = item.title
+    },
+    doneEdit (item) {
+      item.title = this.cacheTodoTitle
+      this.cacheTodoTitle = ''
+      this.cacheTodo = {}
+    },
+    deleteTodos () {
+      this.todos = []
+    },
+    markTodos (item) {
+      this.cacheTodoMark = item.marked
+      return (!this.cacheTodoMark) ? item.marked = true : item.marked = false
+    },
+    addComment () {
+      if (this.cacheTodo.comments === undefined) {
+        this.cacheTodo.comments.push(this.commentText)
+      }
+      if (this.commentText) {
+        this.cacheTodo.comments.unshift(this.commentText)
+      }
+      this.commentText = ''
+    },
+    removeComment (index) {
+      this.cacheTodo.comments.splice(index, 1)
+    }
+  },
+  computed: {
+    filteredTodos () {
+      const newTodo = []
+      if (this.visibility === 'all') {
+        return this.todos
+      } if (this.visibility === 'doing') {
+        this.todos.forEach((item) => {
+          if (!item.completed) {
+            newTodo.push(item)
+          }
+        })
+        return newTodo
+      } if (this.visibility === 'completed') {
+        this.todos.forEach((item) => {
+          if (item.completed) {
+            newTodo.push(item)
+          }
+        })
+        return newTodo
+      }
+    },
+    remaining () {
+      return this.todos.filter(item => !item.completed)
+    }
+  },
+  mounted () {
+    this.getTodos()
+  },
+  watch: {
+    todos: {
+      handler (updatedList) {
+        localStorage.setItem('todos-vuejs', JSON.stringify(updatedList))
+      },
+      deep: true
+    }
+  },
+  created () {
+    this.timeMessage = moment().format('LTS')
+    setInterval(() => this.updateCurrentTime(), 1 * 1000)
+  }
+}
+</script>
