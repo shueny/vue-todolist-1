9ece4345cb8741dbe94fb708bb09a272b9adcdcd
Shueny <shuen29@gmail.com>
2019-05-09 16:39:00 +0800

    update coding

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="home">
     <Todo></Todo>
-    <!-- <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
