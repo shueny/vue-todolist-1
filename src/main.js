5c28f8269edf3e6dd05ccd10d5705d7023bfae94
Shueny <shuen29@gmail.com>
2019-05-09 15:48:38 +0800

    update code

--- src/main.js
+++ src/main.js
@@ -1,13 +1,32 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import './registerServiceWorker';
+import Vue from 'vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faCircle, faMobileAlt, faEnvelope, faEllipsisH, faTrashAlt,
+  faStar, faFileAlt, faCalendarAlt, faEdit
+} from '@fortawesome/free-solid-svg-icons'
+import {
+  faCalendarCheck, faCommentDots
+} from '@fortawesome/free-regular-svg-icons'
+import { faGithub } from '@fortawesome/fontawesome-free-brands'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import BootstrapVue from 'bootstrap-vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './registerServiceWorker'
+import 'bootstrap' // Import js file
+import 'bootstrap/dist/css/bootstrap.min.css' // Import css file
+import 'vue-datetime/dist/vue-datetime.css'
 
-Vue.config.productionTip = false;
-
+library.add(
+  faCalendarCheck, faCommentDots, faCircle, faMobileAlt, faEnvelope, faGithub, faEllipsisH,
+  faTrashAlt, faStar, faFileAlt, faCalendarAlt, faEdit
+)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 new Vue({
   router,
   store,
-  render: h => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount('#app')
