63dec61960f4e71326d880c7e2cc0b4ab2fd3996
Shueny <shuen29@gmail.com>
2019-05-17 12:01:29 +0800

    initial subtree

---
+++ precache-manifest.ced1d5a1ef9c088f3f3d4e8b95feff78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "47519a995ba711b726b4",
+    "url": "/css/app.5984b32e.css"
+  },
+  {
+    "revision": "47519a995ba711b726b4",
+    "url": "/js/app.9f0e4fb7.js"
+  },
+  {
+    "revision": "d8e4aff4387f6ef35290",
+    "url": "/css/chunk-vendors.7425a074.css"
+  },
+  {
+    "revision": "d8e4aff4387f6ef35290",
+    "url": "/js/chunk-vendors.e6748a49.js"
+  },
+  {
+    "revision": "e2b461ac99626eaf85d7a2686e467fca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+];
