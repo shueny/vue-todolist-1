01d665fe840b8a6b42cfec4ff03a8318ec0b3292
Shueny <shuen29@gmail.com>
2019-05-17 14:22:36 +0800

    update dist

--- service-worker.js
+++ service-worker.js
@@ -11,24 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-// eslint-disable-next-line no-undef
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js')
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
-// eslint-disable-next-line no-undef
 importScripts(
-  '/vue-todolist-1/precache-manifest.e363b85fa6df855912a53a3f60e600a9.js'
-)
+  "/vue-todolist-1/precache-manifest.e363b85fa6df855912a53a3f60e600a9.js"
+);
 
-// eslint-disable-next-line no-undef
-workbox.core.setCacheNameDetails({prefix: 'vue-todolist-1'})
+workbox.core.setCacheNameDetails({prefix: "vue-todolist-1"});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-// eslint-disable-next-line no-restricted-globals
-// eslint-disable-next-line no-underscore-dangle
-self.__precacheManifest = [].concat(self.__precacheManifest || [])
-workbox.precaching.suppressWarnings()
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
